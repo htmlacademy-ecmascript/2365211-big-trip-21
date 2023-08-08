eb7d820dd189c753bd82650c60afa64fe829d14a
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-08 19:30:27 +0300

    Настройка хэширования

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js', //имя бандла
+    filename: 'bundle.[contenthash].js', //имя бандла
     path: path.resolve(__dirname, 'build'), //Директория для файлов сборки
     clean: true,
   },
   devtool: 'source-map', // Генерируем карту исходного кода
   plugins: [ // Подключаем плагины
+    new HtmlPlugin({
+      template: 'public/index.html',
+  }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
     }),
   ],
   module: {
