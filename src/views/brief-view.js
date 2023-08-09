57abb847f5c14d8a51c2596b3392be4880859796
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-09 20:56:43 +0300

    Добавлен компонент brief-view

--- src/views/brief-view.js
+++ src/views/brief-view.js
@@ -1,5 +1,5 @@
 import View from './view.js';
- import {html} from '../utilities.js';
+import {html} from '../utilities.js';
 
  class BriefView extends View {
    constructor() {
@@ -13,6 +13,16 @@ import View from './view.js';
     */
    createHtml() {
      return html`
+     <div class="trip-info__main">
+       <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+
+       <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+     </div>
+
+     <p class="trip-info__cost">
+       Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+     </p>
+
        <div class="trip-info__main">
          <h1 class="trip-info__title">Amsterdam — Chamonix — Geneva</h1>
 
