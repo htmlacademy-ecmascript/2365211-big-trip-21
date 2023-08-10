c036f9f500cdc309ac7b737cf01a3762daac58fe
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-10 11:05:14 +0300

    Добавила sort-view.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './views/brief-view.js';
 import './views/filter-view.js';
 import './views/add-button-view.js';
+import './views/sort-view.js';
 
  /**
   * @type {import('./views/brief-view').default}
@@ -9,7 +10,7 @@ import './views/add-button-view.js';
  const briefView = document.querySelector('brief-view');
 
  /**
-  * @type {import('./views/brief-view').default}
+  * @type {import('./views/filter-view').default}
   */
 
  const filterView = document.querySelector('filter-view');
@@ -19,7 +20,12 @@ import './views/add-button-view.js';
   */
  const addButtonView = document.querySelector('add-button-view');
 
+ /**
+  * @type {import('./views/sort-view').default}
+  */
+ const sortView = document.querySelector('sort-view');
 
  briefView.render();
  filterView.render();
  addButtonView.render();
+ sortView.render();
