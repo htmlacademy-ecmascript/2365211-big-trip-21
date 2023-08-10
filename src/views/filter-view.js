aea61cf361ce4bcbce3f06ead5a5724c12a32819
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-10 10:29:47 +0300

    добавлен filter-view

--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -1,23 +1,41 @@
-<form class="trip-filters" action="#" method="get">
-                <div class="trip-filters__filter">
-                  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked="">
-                  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-                </div>
+import View from './view.js';
+ import {html} from '../utilities.js';
 
-                <div class="trip-filters__filter">
-                  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-                  <label class="trip-filters__filter-label" for="filter-future">Future</label>
-                </div>
+ class FilterView extends View {
+   constructor() {
+     super();
 
-                <div class="trip-filters__filter">
-                  <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-                  <label class="trip-filters__filter-label" for="filter-present">Present</label>
-                </div>
+     this.classList.add('trip-filters');
+   }
 
-                <div class="trip-filters__filter">
-                  <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-                  <label class="trip-filters__filter-label" for="filter-past">Past</label>
-                </div>
+   /**
+    * @override
+    */
+   createHtml() {
+     return html`
+       <div class="trip-filters__filter">
+         <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked="">
+         <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+       </div>
 
-                <button class="visually-hidden" type="submit">Accept filter</button>
-              </form>
+       <div class="trip-filters__filter">
+         <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+         <label class="trip-filters__filter-label" for="filter-future">Future</label>
+       </div>
+
+       <div class="trip-filters__filter">
+         <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
+         <label class="trip-filters__filter-label" for="filter-present">Present</label>
+       </div>
+
+       <div class="trip-filters__filter">
+         <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+         <label class="trip-filters__filter-label" for="filter-past">Past</label>
+       </div>
+     `;
+   }
+ }
+
+ customElements.define('filter-view', FilterView);
+
+ export default FilterView;
