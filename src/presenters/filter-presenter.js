9154ff69524cd7d80f6114cdcd4eb3e2fb8ef32a
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-20 00:49:36 +0300

    В  filter-view завели данные

--- src/presenters/filter-presenter.js
+++ src/presenters/filter-presenter.js
@@ -19,8 +19,19 @@ class FilterPresenter extends Presenter {
   /**
    * @override
    */
+
   updateView() {
-    this.view.render();
+    /**
+   * @type {Array<FilterType>}
+   */
+    const values = ['everything', 'future', 'present', 'past'];
+    const items = values.map((value) => ({
+      value,
+      isSelected: value === 'everything',
+      isDisabled: value === 'future'
+    }));
+    //this.view.render();
+    this.view.setState({items});
   }
 }
 
--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -1,7 +1,7 @@
 import View from './view.js';
 import {html} from '../utilities.js';
 
-  /**
+/**
   * @typedef {{
   *   value: FilterType
   *   isSelected: boolean
@@ -14,34 +14,32 @@ import {html} from '../utilities.js';
   *
   * @extends {View<State>}
   */
-  class FilterView extends View {
-    constructor() {
-      super();
+class FilterView extends View {
+  constructor() {
+    super();
 
-      this.classList.add('trip-filters');
-    }
+    this.classList.add('trip-filters');
+  }
 
   createHtml() {
     return html`
-       <div class="trip-filters__filter">
-         <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked="">
-         <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-       </div>
+    ${this.state.items.map((item) => html`
 
        <div class="trip-filters__filter">
-         <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-         <label class="trip-filters__filter-label" for="filter-future">Future</label>
+         <input
+         id="filter-everything"
+         class="trip-filters__filter-input  visually-hidden"
+         type="radio"
+         name="trip-filter"
+         value="everything"
+         checked="">
+         <label
+         class="trip-filters__filter-label"
+         for="filter-everything">
+         Everything
+         </label>
        </div>
-
-       <div class="trip-filters__filter">
-         <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-         <label class="trip-filters__filter-label" for="filter-present">Present</label>
-       </div>
-
-       <div class="trip-filters__filter">
-         <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-         <label class="trip-filters__filter-label" for="filter-past">Past</label>
-       </div>
+       `)}
      `;
   }
 }
