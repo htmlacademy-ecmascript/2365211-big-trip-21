48103759c81f13add053747759caf00e4d3645a2
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-22 15:46:57 +0300

    filer-view is working

--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -17,31 +17,31 @@ import {html} from '../utilities.js';
 class FilterView extends View {
   constructor() {
     super();
-
     this.classList.add('trip-filters');
   }
 
   createHtml() {
     return html`
-    ${this.state.items.map((item) => html`
-    <div class="trip-filters__filter">
-    <input
-    id="filter-${item.value}"
-    class="trip-filters__filter-input  visually-hidden"
-    type="radio"
-    name="trip-filter"
-    value="${item.value}"
-    ${item.isSelected ? 'checked' : ''}
-    ${item.isSelected ? 'checked' : ''}>
-    <label
-    class="trip-filters__filter-label"
-    for="filter-${item.value}">
-    ${item.value}
-    </label>
-  </div>
-       `)}
-     `;
+      ${this.state.items.map((item) => html`
+
+        <div class="trip-filters__filter">
+          <input
+            id="filter-${item.value}"
+            class="trip-filters__filter-input  visually-hidden"
+            type="radio"
+            name="trip-filter"
+            value="${item.value}"
+            checked="${item.isSelected ? 'checked' : ''}">
+          <label
+            class="trip-filters__filter-label"
+            for="filter-${item.value}">
+            ${item.value}
+          </label>
+        </div>
+      `)}
+    `;
   }
+
 }
 
 customElements.define('filter-view', FilterView);
