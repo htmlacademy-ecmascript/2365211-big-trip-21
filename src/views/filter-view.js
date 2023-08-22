a18b3809302124eb558b1eb2bbf2a13a3ec6eb9f
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-22 13:39:06 +0300

    Передали данные в sort-view

--- src/views/filter-view.js
+++ src/views/filter-view.js
@@ -24,21 +24,20 @@ class FilterView extends View {
   createHtml() {
     return html`
     ${this.state.items.map((item) => html`
-
-       <div class="trip-filters__filter">
-         <input
-         id="filter-everything"
-         class="trip-filters__filter-input  visually-hidden"
-         type="radio"
-         name="trip-filter"
-         value="everything"
-         checked="">
-         <label
-         class="trip-filters__filter-label"
-         for="filter-everything">
-         Everything
-         </label>
-       </div>
+    <div class="trip-filters__filter">
+    <input
+    id="filter-${item.value}"
+    class="trip-filters__filter-input  visually-hidden"
+    type="radio"
+    name="trip-filter"
+    value="${item.value}"
+    checked="${item.isSelected ? 'checked' : ''}">
+    <label
+    class="trip-filters__filter-label"
+    for="filter-${item.value}">
+    ${item.value}
+    </label>
+  </div>
        `)}
      `;
   }
