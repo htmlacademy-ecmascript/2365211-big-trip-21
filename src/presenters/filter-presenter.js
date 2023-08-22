57811c66a672201858a67c637d3b88138c3a4c67
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-08-22 15:45:46 +0300

    filter-presenter is done

--- src/presenters/filter-presenter.js
+++ src/presenters/filter-presenter.js
@@ -13,26 +13,36 @@ class FilterPresenter extends Presenter {
   constructor(...rest) {
     super(...rest);
 
-    // this.view.addEventListener('change', this.onViewChange.bind(this));
+    this.view.addEventListener('change', this.onViewChange.bind(this));
   }
 
   /**
    * @override
    */
-
   updateView() {
     /**
-   * @type {Array<FilterType>}
-   */
+     * @type {Array<FilterType>}
+     */
     const values = ['everything', 'future', 'present', 'past'];
     const items = values.map((value) => ({
       value,
-      isSelected: value === 'everything',
+      isSelected: value === 'past',
       isDisabled: value === 'future'
     }));
+
     //this.view.render();
+    console.log(items);
     this.view.setState({items});
   }
+
+  onViewChange(event) {
+console.log(event.target.value)
+
+    // const params = this.navigation.getParams();
+    // params.filter = event.target.state.value;
+    // this.navigation.setParams(params);
+  }
+
 }
 
 export default FilterPresenter;
