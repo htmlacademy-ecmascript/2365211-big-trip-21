c142d1148a315393cef34ec197a1e1a920664072
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-09-18 12:42:53 +0300

    Реализуем отрисовку FilterView с учётом навигации

--- src/models/app-model.js
+++ src/models/app-model.js
@@ -24,6 +24,16 @@ class AppModel extends Model {
     this.offerGroups = [];
 
     /**
+     * @type {Record<FilterType, (point: PointModel) => boolean>}
+     */
+    this.filterCallbacks = {
+      everything: () => true,
+      future: (point) => point.dateFromInMs > Date.now(),
+      present: (point) => point.dateFromInMs <= Date.now() && point.dateToInMs >= Date.now(),
+      past: (point) => point.dateToInMs < Date.now()
+    };
+
+    /**
      * @type {Record<SortType, (pointA: PointModel, pointB: PointModel) => number>}
      */
     this.sortCallBacks = {
@@ -53,13 +63,21 @@ class AppModel extends Model {
 
   /**
    * @param {{sort?: SortType}} options
+   * @param {{
+  *  filter?: FilterType
+  *  sort?: SortType
+  * }} options
+  *
    * @returns {Array<PointModel>}
    */
   getPoints(options = {}) {
+    const defaultFilter = this.filterCallbacks.everything;
+    const filter = this.filterCallbacks[options.filter] ?? defaultFilter;
     const defaultSort = this.sortCallBacks.day;
     const sort = this.sortCallBacks[options.sort] ?? defaultSort;
 
     return this.points.map(this.createPoint).sort(sort);
+    return this.points.map(this.createPoint).filter(filter).sort(sort);
   }
 
   /**
