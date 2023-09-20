4c80dba7153c698a81d0b59a7da645bea8bd6cfa
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-09-20 14:51:06 +0300

    Реализуем в EditorView отправку события delete

--- src/views/editor-view.js
+++ src/views/editor-view.js
@@ -342,8 +342,9 @@ class EditorView extends View {
    * @param {Event} event
    */
   onReset(event) {
+    const {id} = this.state;
     event.preventDefault();
-    this.dispatch('close');
+    this.dispatch(id === 'draft' ? 'close' : 'delete');
   }
 
 }
